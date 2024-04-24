44d33ee69c7a0a5d4c6e742a7105243bdc060720
Danilo dos Santos <danilo.santossouza@outlook.com>
2024-04-23 22:37:35 -0300

    Constroi base do projeto

---
+++ Petfolio.Application/UseCases/Pets/Update/UpdatePetUserCase.cs
@@ -0,0 +1,11 @@
+﻿using Petfolio.Communication.Requests;
+
+namespace Petfolio.Application.UseCases.Pets.Update;
+
+public class UpdatePetUserCase
+{
+    public void Execute(int id, RequestRegisterPetJson request)
+    {
+        
+    }
+}
